fb574ce04238414f3a6246d67b3911399665e6cb
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2022-11-15 18:26:01 +0300

    Add react files

---
+++ src/components/Calculator.js
@@ -0,0 +1,54 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/self-closing-comp */
+
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="display">
+            <span></span>
+            {' '}
+            0
+          </div>
+
+          <div className="digits">
+            <button className="btns">AC</button>
+            <button className="btns">+/-</button>
+            <button className="btns">%</button>
+            <button className="btns amber">÷</button>
+
+            <button className="btns-1">7</button>
+            <button className="btns-1">8</button>
+            <button className="btns-1">9</button>
+            <button className="btns amber">X</button>
+
+            <button className="btns">4</button>
+            <button className="btns">5</button>
+            <button className="btns">6</button>
+            <button className="btns amber">-</button>
+          
+            <button className="btns">1</button>
+            <button className="btns">2</button>
+            <button className="btns">3</button>
+            <button className="btn2 amber">+</button>
+
+            <button className="btns-2">0</button>
+            <button className="btns">.</button>
+            <button className="btns amber">=</button>
+          
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
